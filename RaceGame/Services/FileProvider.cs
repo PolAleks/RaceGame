28c8eeeaec5b1d23c02d9be1373702fe04e425e9
PolAleks <aa.polushkin@yandex.ru>
2025-10-04 12:14:45 +0300

    Реализовал слой отвечающий за работу с файловой системой

---
+++ RaceGame/Services/FileProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace Race.Services
+{
+    public class FileProvider
+    {
+        private static string path;
+        public FileProvider()
+        {
+            path = Environment.CurrentDirectory;
+        }
+
+        public static string Load(string file)
+        {
+            file = Path.Combine(path, file);
+            return File.ReadAllText(file, Encoding.UTF8);
+        }
+
+        public static void Save(string file, string content)
+        {
+            file = Path.Combine(path, file);
+            File.WriteAllText(file, content, Encoding.UTF8);
+        }
+    }
+}
