180f0c549ecb6e058e2f8d0cc721d083fbe9b8a3
PolAleks <aa.polushkin@yandex.ru>
2025-10-04 12:16:03 +0300

    Реализован слой преобразования результатов игры в json формат

---
+++ RaceGame/Services/IConvert.cs
@@ -0,0 +1,8 @@
+﻿namespace Race.Services
+{
+    public interface IConvert
+    {
+        string Serialize<T>(T value);
+        T Deserialize<T>(string value);
+    }
+}
---
+++ RaceGame/Services/JsonConverter.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Race.Services
+{
+    public class JsonConverter : IConvert
+    {
+        public string Serialize<T>(T value)
+        {
+            return JsonConvert.SerializeObject(value, Formatting.Indented);
+        }
+
+        public T Deserialize<T>(string value)
+        {
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
